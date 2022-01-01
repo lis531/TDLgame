6139e847e70efb53c52c1ba32742b2889ee2eb3c
lis531 <lisekminecraftowy175@gmail.com>
2022-01-01 19:42:41 +0100

    .

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chodzenie : MonoBehaviour
+public class Moving : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 10.0f;
+    //when button is pressed move the object forward
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+
+    //camera follow object
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
     }
 }
