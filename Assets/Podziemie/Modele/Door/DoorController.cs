2e0115e58e59d6b71ae7a13d42dbdf5ae8aa528a
GameWin221 <mateusz.gamewin1@gmail.com>
2022-01-02 21:37:32 +0100

    Dźwięki chodzenia i drzwi

--- Assets/Podziemie/Modele/Door/DoorController.cs
+++ Assets/Podziemie/Modele/Door/DoorController.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     Animation anim;
+    AudioSource aSource;
+    GameObject player;
 
     bool isOpen = false;
+
+    const float interactionDistance = 2.0f;
 
     private void Start()
     {
         anim = transform.GetComponent<Animation>();
+        aSource = transform.GetComponent<AudioSource>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     public void Open()
@@ -17,6 +23,9 @@ public class DoorController : MonoBehaviour
         anim.clip = anim.GetClip("Open");
         anim.Play();
 
+        aSource.Stop();
+        aSource.Play();
+
         isOpen = true;
     }
     public void Close()
@@ -24,6 +33,9 @@ public class DoorController : MonoBehaviour
         anim.Stop();
         anim.clip = anim.GetClip("Close");
         anim.Play();
+
+        aSource.Stop();
+        aSource.Play();
 
         isOpen = false;
     }
@@ -42,6 +54,8 @@ public class DoorController : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown("e"))
-            ChangeState();
+            if(Vector3.Distance(transform.position, player.transform.position) < interactionDistance &&
+               Vector3.Dot(player.transform.forward, (transform.position - player.transform.position).normalized) > 0.65)
+                ChangeState();
     }
 }
--- Assets/Podziemie/TunnelMoving.cs
+++ Assets/Podziemie/TunnelMoving.cs
@@ -1,32 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class TunnelMoving : MonoBehaviour
 {
     const float sensitivity = 200.0f;
-    const float speed = 10.0f;
+    const float speed = 3.5f;
 
-    CharacterController character;
-    private GameObject camera;
+    private CharacterController character;
+    private GameObject cam;
+
+    float targetCamRot = 0;
+
+    AudioSource aSource;
+    public AudioClip[] stepSounds;
+    public float stepOffset = 0.5f;
+    bool isStepping = false;
+
+    bool PlayerMoves()
+    {
+        if (Input.GetAxis("Vertical") + Input.GetAxis("Horizontal") != 0)
+            return true;
+        else return false;
+    }
 
     void Start()
     {
         character = gameObject.GetComponent<CharacterController>();
-        camera = transform.GetChild(0).gameObject;
+        aSource = gameObject.GetComponent<AudioSource>();
+        cam = transform.GetChild(0).gameObject;
 
         Cursor.lockState = CursorLockMode.Locked;
     }
 
+    IEnumerator PlayStep()
+    {
+        isStepping = true;
+
+        AudioClip clip = stepSounds[Random.Range(0, stepSounds.Length)];
+        aSource.PlayOneShot(clip);
+
+        aSource.pitch = Random.Range(0.9f, 1.1f);
+        aSource.volume = Random.Range(0.75f, 1.0f);
+
+        yield return new WaitForSeconds(stepOffset);
+
+        if (PlayerMoves())
+            StartCoroutine(PlayStep());
+        else
+            isStepping = false;
+    }
+
     void Update()
     {
+        // Poruszanie sie postaci
         character.Move(
             ((Input.GetAxis("Vertical") * transform.forward) + // Przod / Tyl
             (Input.GetAxis("Horizontal") * transform.right)) * // Lewo / Prawo
             speed * Time.deltaTime                           // Skalowanie
         );
 
+        if (PlayerMoves() && !isStepping)
+            StartCoroutine(PlayStep());
+
+        // Obrot postaci lewo/prawo
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime, 0));
-        camera.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * -sensitivity * Time.deltaTime, 0, 0));
+
+        // Ruch kamera gora/dol
+        targetCamRot += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        targetCamRot = Mathf.Clamp(targetCamRot, -89.99f, 89.99f);
+
+        cam.transform.localRotation = Quaternion.Euler(-targetCamRot, 0, 0);
     }
 }
