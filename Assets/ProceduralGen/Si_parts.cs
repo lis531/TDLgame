9c9e225cd5ce65a4f61ed127d9be9725547c21d4
lis531 <lisekminecraftowy175@gmail.com>
2022-01-02 13:19:11 +0100

    .

--- Assets/ProceduralGen/Si_parts.cs
+++ Assets/ProceduralGen/Si_parts.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.GameObject;
 
 public class Si_parts : MonoBehaviour
 {
     GameObject Player;
+    GameObject Wall;
     // Start is called before the first frame update
     void Start()
     {
+        GameObject.Find(Wall);
         //if object is not near to the player object then teleport it near to the player object
         if (Vector3.Distance(transform.position, GameObject.Find("Player").transform.position) > 10)
         {
             transform.position = GameObject.Find("Player").transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
         }
     }
+    //can't see player through the wall
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Player = other.gameObject;
+            Player.GetComponent<Player_move>().can_see = false;
+        }
+    }
+    //can't go throught closed door
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Player = other.gameObject;
+            Player.GetComponent<Player_move>().can_see = false;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
