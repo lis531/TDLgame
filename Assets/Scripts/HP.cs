5a2fd9446778e490368dfcdd0e0c82346721a39d
GameWin221 <mateusz.gamewin1@gmail.com>
2022-01-03 18:55:32 +0100

    Nowe Tekstury, nowy Enemy, HP

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -1,25 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class HP : MonoBehaviour
 {
-    //create health variable
-    public int health = 100;
-    //create variable for the gameobject
-    public GameObject Player;
-    public GameObject Enemy;
-    //if enemy touching the Player then Player loses 10 health every second
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            health -= 10;
-            if (health <= 0)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
-        }
+    public float health = 100.0f;
+
+    public void DealDamage(float damage)
+    { 
+        health -= damage;
+        if (health <= 0)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
+        
     }
 }
--- Assets/Scripts/Si_parts.cs
+++ Assets/Scripts/Si_parts.cs
@@ -5,29 +5,55 @@ using UnityEngine;
 public class Si_parts : MonoBehaviour
 {
     public GameObject Player;
+    HP hp;
+
     public GameObject Wall;
-    public GameObject Enemy;
     public GameObject Door;
+
+    private bool canAttack = true;
+    public float attackCooldown = 1.0f;
+    public float attackDistance = 2.0f;
+    public float attackDamage = 10.0f;
+
+    public float movementSpeed = 10.0f;
+
+    IEnumerator DealDamage()
+    {
+        // Deal Damage
+        hp.DealDamage(attackDamage);
+
+        // Cooldown Begin
+        canAttack = false;
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+        // Cooldown End
+    }
+
     void Start()
     {
+        hp = Player.GetComponent<HP>();
+
         //if Enemy isn's near to the Player then teleport Enemy near to the Player
         if (Vector3.Distance(transform.position, GameObject.Find("Player").transform.position) > 100)
-        {
             transform.position = GameObject.Find("Player").transform.position + new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
-        }
+        
     }
     //Enemy can't see throught door 
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Door")
-        {
-            Enemy.transform.position = new Vector3(Player.transform.position.x + 5, Player.transform.position.y, Player.transform.position.z);
-        }
+            transform.position = new Vector3(Player.transform.position.x + 5, Player.transform.position.y, Player.transform.position.z);
+        
     }
-    //slowly move Enemy to the Player
+
     void Update()
     {
-        Enemy.transform.position = Vector3.MoveTowards(Enemy.transform.position, Player.transform.position, 0.1f);
+        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, movementSpeed * Time.deltaTime);
+
+        if (canAttack)
+        {
+            if (Vector3.Distance(transform.position, Player.transform.position) < attackDistance)
+                StartCoroutine(DealDamage());
+        }
     }
-    //Enemy can't see player and go through the wall
 }
