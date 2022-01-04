502d5dc8265526237cb9b8e2d8e54a43033e547d
GameWin221 <mateusz.gamewin1@gmail.com>
2022-01-04 09:52:11 +0100

    Nowe menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,35 +4,69 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject elevator;
+    public GameObject mCamera;
+    public GameObject fadeOut;
+    public GameObject options;
+
     Animation elevatorAnim;
     Animation cameraAnim;
     Animation menuAnim;
+    Animation fadeOutAnim;
+    Animation optionsAnim;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+        elevatorAnim = elevator.GetComponent<Animation>();
+        cameraAnim = mCamera.GetComponent<Animation>();
+        menuAnim = transform.GetComponent<Animation>();
+        fadeOutAnim = fadeOut.GetComponent<Animation>();
+        optionsAnim = options.GetComponent<Animation>();
+    }
 
     IEnumerator FlyAndLoad()
     {
-        elevatorAnim = GameObject.Find("Elevator").GetComponent<Animation>();
-        cameraAnim = GameObject.Find("Main Camera").GetComponent<Animation>();
-        menuAnim = transform.GetComponent<Animation>();
-
         cameraAnim.Play();
         elevatorAnim.Play();
         menuAnim.Play();
+        fadeOutAnim.Play();
 
         yield return new WaitForSeconds(cameraAnim.clip.length);
         SceneManager.LoadScene("Podziemie");
     }
+
     public void PlayGame ()
     {
         StartCoroutine(FlyAndLoad());
     }
 
+    public void GoToOptions()
+    {
+        cameraAnim.clip = cameraAnim.GetClip("GoToOptions");
+        cameraAnim.Play();
+
+        menuAnim.clip = menuAnim.GetClip("DisappeaMenu");
+        menuAnim.Play();
+
+        optionsAnim.clip = optionsAnim.GetClip("SettingsAppear");
+        optionsAnim.Play();
+    }
+    public void GoBackToMenu()
+    {
+        cameraAnim.clip = cameraAnim.GetClip("GoToMenu");
+        cameraAnim.Play();
+
+        menuAnim.clip = menuAnim.GetClip("AppearMenu");
+        menuAnim.Play();
+
+        optionsAnim.clip = optionsAnim.GetClip("SettingsDisappear");
+        optionsAnim.Play();
+    }
+
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.None;
     }
 }
