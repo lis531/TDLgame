d13ab35922047a278aa84197492458912c1a638b
GameWin221 <mateusz.gamewin1@gmail.com>
2022-01-06 12:18:25 +0100

    Menu reflection probes

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -30,7 +30,6 @@ public class MainMenu : MonoBehaviour
     {
         cameraAnim.clip = cameraAnim.GetClip("FlyToElevator");
         menuAnim.clip = menuAnim.GetClip("DisappeaMenu");
-        //optionsAnim.clip = optionsAnim.GetClip("SettingsAppear");
 
         cameraAnim.Play();
         elevatorAnim.Play();
