c8eccfa8d49098edddcb2a366c14d21659ef01c8
GameWin221 <mateusz.gamewin1@gmail.com>
2022-01-06 11:19:56 +0100

    Ładowanie ustawień

--- Assets/Menu/SoundsSettings.cs
+++ Assets/Menu/SoundsSettings.cs
@@ -3,13 +3,25 @@ using UnityEngine.UI;
 
 public class SoundsSettings : MonoBehaviour
 {
-    public void UpdateSFXVolume(Slider slider)
+    Slider sfxSlider;
+    Slider musicSlider;
+
+    public void UpdateSFXVolume()
     {
-        PlayerPrefs.SetFloat("SFXVolume", slider.value);
+        PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
     }
    
-    public void UpdateMusicVolume(Slider slider)
+    public void UpdateMusicVolume()
     {
-        PlayerPrefs.SetFloat("MusicVolume", slider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+    }
+
+    private void Start()
+    {
+        sfxSlider = transform.GetChild(0).GetComponent<Slider>();
+        musicSlider = transform.GetChild(1).GetComponent<Slider>();
+
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
     }
 }
