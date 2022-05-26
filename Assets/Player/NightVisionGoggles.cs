7131cb4b24b8a2cdad20a2e0632029954206e8d5
lis531 <lisekminecraftowy175@gmail.com>
2022-05-26 12:54:04 +0200

    Nocna wizja

--- Assets/Player/NightVisionGoggles.cs
+++ Assets/Player/NightVisionGoggles.cs
@@ -1,30 +1,33 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class NightVisionGoggles : MonoBehaviour
 {
     bool TurnedOn;
-    Exposure exposure;
-    void Start()
-    {
-        GlobalVolume volume = gameObject.GetComponent<Exposure>();
-        Exposure tmp;
-        if (volume.profile.TryGet<Exposure>(out tmp))
-        {
-            exposure = tmp;
-        }
-    }
+    public Volume volume;
+    public VolumeProfile NightVisonProfile;
+    public VolumeProfile GlobalVolumeProfile;
     void Update()
     {
         if (Input.GetKeyDown("n") && PlayerInventory.hasGoggles && !TurnedOn)
         {
-            //change exposure settings to 8
-            exposure.enabled.value = true;
-            exposure.fixedExposure.value = 8;
             TurnedOn = true;
         }
         else if (Input.GetKeyDown("n") && TurnedOn)
         {
             TurnedOn = false;
         }
+        //if (TurnedOn) load profile NightVision
+        //else load profile GlobalVolume
+        if (TurnedOn)
+        {
+            volume.profile = NightVisonProfile;
+        }
+        else
+        {
+            volume.profile = GlobalVolumeProfile;
+        }
+
     }
 }
