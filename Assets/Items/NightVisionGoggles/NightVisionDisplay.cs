87d44ff681a2b8be7db95536d79f451633a506a6
lis531 <lisekminecraftowy175@gmail.com>
2022-06-03 15:07:54 +0200

    fix noktowizji

--- Assets/Items/NightVisionGoggles/NightVisionDisplay.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class NightVisionDisplay : MonoBehaviour
-{
-    void Update()
-    {
-        transform.localScale = new Vector3(PlayerInventory.hasGoggles ? 1f : 0f, 1f, 1f);
-    }
-}
--- Assets/Items/NightVisionGoggles/Noktowizja.cs
+++ Assets/Items/NightVisionGoggles/Noktowizja.cs
@@ -5,10 +5,10 @@ public class Noktowizja : MonoBehaviour
 {
     public GameObject enemycialo;
     public GameObject enemy;
-    public GameObject localVolume;
+    public GameObject m_localVolume;
     public Volume volume;
-    public VolumeProfile m_NightVisonProfile;
-    public VolumeProfile m_GlobalVolumeProfile;
+    public VolumeProfile NightVisonProfile;
+    public VolumeProfile GlobalVolumeProfile;
     public static bool m_TurnedOn = false;
     
     void Update()
@@ -18,16 +18,16 @@ public class Noktowizja : MonoBehaviour
             if (Input.GetKeyDown("n") && !m_TurnedOn)
             {
                 m_TurnedOn = true;
-                localVolume.SetActive(true);
-                volume.profile = m_NightVisonProfile;
+                m_localVolume.SetActive(true);
+                volume.profile = NightVisonProfile;
                 enemy.GetComponent<AudioSource>().volume = 1;
                 enemycialo.transform.localScale = new Vector3(70f, 70f, 60f);
             }
             else if (Input.GetKeyDown("n") && m_TurnedOn)
             {
                 m_TurnedOn = false;
-                localVolume.SetActive(false);
-                volume.profile = m_GlobalVolumeProfile;
+                m_localVolume.SetActive(false);
+                volume.profile = GlobalVolumeProfile;
                 enemy.GetComponent<AudioSource>().volume = 0;
                 enemycialo.transform.localScale = new Vector3(0f, 0f, 0f);
             }
